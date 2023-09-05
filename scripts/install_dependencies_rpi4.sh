9d91f5693a72c654a7a61d86407e8f37ca867bd9
Joshua <jspri17@gmail.com>
2023-09-05 02:11:03 +0000

    Changing dependencies script remotely.

--- scripts/install_dependencies_rpi4.sh
+++ scripts/install_dependencies_rpi4.sh
@@ -2,3 +2,7 @@
 
 apt-get update
 apt-get install vim git mlocate tree htop dnsmasq cmake ffmpeg libavcodec-dev libavformat-dev libavfilter-dev libopus-dev libusb-1.0-0-dev libopencv-dev
+
+sudo updatedb
+
+ln -s /usr/lib/arm-linux-gnueabihf/libopus.a /usr/local/lib/libopus.a
