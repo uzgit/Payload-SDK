c84b131b726c4c4fd4456e45a83d480b3d46f8f9
Joshua <jspri17@gmail.com>
2023-09-05 01:57:58 +0000

    Added dependencies script for rpi4.

---
+++ scripts/install_dependencies_rpi4.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt-get update
+apt-get install vim git mlocate tree htop dnsmasq cmake ffmpeg libavcodec-dev libavformat-dev libavfilter-dev libopus-dev libusb-1.0-0-dev libopencv-dev
