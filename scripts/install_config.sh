fb41ce88862b9d83b1907df8305f28b5bf9f4bd2
Joshua <jspri17@gmail.com>
2023-09-19 17:50:11 +0000

    Added -y flag to apt installation command.

--- scripts/install_config.sh
+++ scripts/install_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install vim git mlocate tree htop dnsmasq cmake ffmpeg libavcodec-dev libavformat-dev libavfilter-dev libopus-dev libusb-1.0-0-dev libopencv-dev
+apt-get install -y vim git mlocate tree htop dnsmasq cmake ffmpeg libavcodec-dev libavformat-dev libavfilter-dev libopus-dev libusb-1.0-0-dev libopencv-dev
 
 sudo updatedb
 
