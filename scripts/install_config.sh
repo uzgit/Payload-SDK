ec3b17b3d4e9368f57455486fce525062b4bffa0
Joshua <jspri17@gmail.com>
2023-09-21 23:00:18 +0000

    Removed comment and ll alias because it has not been working.

--- scripts/install_config.sh
+++ scripts/install_config.sh
@@ -6,12 +6,9 @@ apt-get install -y vim git mlocate tree htop dnsmasq cmake ffmpeg libavcodec-dev
 sudo updatedb
 
 ln -s /usr/lib/arm-linux-gnueabihf/libopus.a /usr/local/lib/libopus.a
-#ln -s /home/joshua/git/Payload-SDK/scripts/usb.sh /root/usb.sh
 
 sed -i 's/rootwait/rootwait modules-load=dwc2,g_ether/' /boot/cmdline.txt
 
 echo "dtoverlay=dwc2"  >> /boot/config.txt
 echo "dtoverlay=uart0" >> /boot/config.txt
 echo "enable_uart=1"   >> /boot/config.txt
-
-echo "alias ll='ls -lah'" >> ~/.bashrc
